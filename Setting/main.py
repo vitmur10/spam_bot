80bac8d735f10ed57ebc74eed99f1877224547cb
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2025-04-20 05:33:31 +0300

    morphology

--- Setting/main.py
+++ Setting/main.py
@@ -1,11 +1,12 @@
 import asyncio
 
 from const import *
-from filters import router, auto_unban_unmute
+from filters import router, auto_moderation_loop, auto_ban_users
 
 
 async def main():
-    asyncio.create_task(auto_unban_unmute(bot))
+    asyncio.create_task(auto_moderation_loop(bot))
+    asyncio.create_task(auto_ban_users())
     dp.include_router(router)
     await dp.start_polling(bot)
 
