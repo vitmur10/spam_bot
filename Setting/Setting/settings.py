2d2e59d317104d18669d4ab9d15fb0a10369ebaf
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2025-11-21 03:49:12 +0200

    fix(settings): remove timeout setting from template context processors

--- Setting/Setting/settings.py
+++ Setting/Setting/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
-            'timeout': 20,
         },
     },
 ]
